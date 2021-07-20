0c4a33adde02b53dbb28516702b32b3c5509c8dc
flavionevesfsn <flavioneves89@gmail.com>
2021-07-20 20:29:36 -0300

    Aplicação do método privado no set

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,16 +3,14 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Flavio Neves"
     var cpf: String = "123.456.789-10"
-
-    inner class Endereco {
-        var rua: String = "Rua teste"
-    }
+    private set
 }
 
 fun main() {
 
     val flavio = Pessoa()
 
+    println(flavio)
     println(flavio.nome)
     println(flavio.cpf)
 

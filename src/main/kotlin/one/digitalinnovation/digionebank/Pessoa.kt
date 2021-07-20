07400b4f73695353f6d3310c2cfae1694917220d
flavionevesfsn <flavioneves89@gmail.com>
2021-07-20 20:49:36 -0300

    Uso da classe members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,6 +4,10 @@ class Pessoa {
     var nome: String = "Flavio Neves"
     var cpf: String = "123.456.789-10"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
@@ -11,8 +15,8 @@ fun main() {
     val flavio = Pessoa()
 
     println(flavio)
-    println(flavio.nome)
-    println(flavio.cpf)
+    println(flavio.pessoaInfo())
+
 
     }
 

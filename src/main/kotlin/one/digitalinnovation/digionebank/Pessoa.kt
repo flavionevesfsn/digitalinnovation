f2f89492365310829cbc1f7596be7d318b9feab4
flavionevesfsn <flavioneves89@gmail.com>
2021-07-19 23:26:33 -0300

    initial commit

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    var nome: String = "Flavio1"
+    var cpf: String = "123.456.789-10"
+}
+
+fun main() {
+
+    val Flavio = Pessoa()
+
+    println(Flavio.nome)
+    println(Flavio.cpf)
+}
+
